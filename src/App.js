14222e569170d73b25e71f22a07728df87ff1e4a
Aditya Joshi <adijoshi82812@pop-os.localdomain>
2021-08-04 20:04:02 +0530

    Added routes and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,54 +1,31 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-
-import { actionCreators } from "./store";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const App = () => {
   const logged_in = useSelector((state) => state.logged_in);
-  const username = useSelector((state) => state.username);
-  const dispatch = useDispatch();
-
-  const [localUser, setLocalUser] = React.useState("");
-
-  const { loginHandler, userHandler } = bindActionCreators(
-    actionCreators,
-    dispatch
-  );
 
   return (
-    <div>
-      <h1>Logged in = {logged_in ? "True" : "False"}</h1>
-      <h1>Username = {username}</h1>
-      <input
-        type="text"
-        name="username"
-        value={localUser}
-        onChange={(e) => {
-          e.preventDefault();
-          setLocalUser(e.target.value);
-        }}
-      />
-      <button
-        type="button"
-        onClick={(e) => {
-          e.preventDefault();
-          userHandler(localUser);
-        }}
-      >
-        Toggle Usename
-      </button>
-      <br />
+    <Router>
+      {logged_in ? <Redirect to="/dashboard/" /> : <Redirect to="/" />}
+      <Switch>
+        <Route exact path="/">
+          Login
+        </Route>
 
-      <button
-        type="button"
-        onClick={(e) => {
-          e.preventDefault();
-          loginHandler(logged_in ? false : true);
-        }}
-      >
-        Toggle Login
-      </button>
-    </div>
+        <Route exact path="/signup/">
+          Sign Up
+        </Route>
+
+        <Route exact path="/dashboard/">
+          DashBoard
+        </Route>
+      </Switch>
+    </Router>
   );
 };
