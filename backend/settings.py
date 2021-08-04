a351964ff501ff4e383f8eeabac676bde7f6969d
Aditya Joshi <adijoshi82812@pop-os.localdomain>
2021-08-04 19:16:04 +0530

    Started the core app

--- backend/settings.py
+++ backend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'rest_framework',
     'corsheaders',
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
