eb208b904bc1a0d5e776d894cd43442ce1c79cf2
Aditya Joshi <adijoshi82812@pop-os.localdomain>
2021-08-05 08:35:36 +0530

    Fixed the backend signup issue

--- core/serializers.py
+++ core/serializers.py
@@ -22,6 +22,17 @@ class UserSerializerWithToken(serializers.ModelSerializer):
 
         return token
 
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
+
     class Meta:
         model = User
         fields = ('token', 'password', 'username')
